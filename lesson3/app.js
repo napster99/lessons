4dcc4ef0f12e2fd749bb0c26ab01750d511b03aa
napster99 <morrisonleisure@163.com>
2015-01-26 20:57:01 +0800

    add lessons code

---
+++ lesson3/app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var cheerio = require('cheerio');
+var superagent = require('superagent');
+
+var app = express();
+
+var netUrl = 'https://cnodejs.org/';
+
+app.get('/',  function(req, res, next) {
+  var q = req.query.q || netUrl;
+  superagent.get(q)
+  .end(function(err, sres) {
+    if(err) {
+      return next(err);
+    }
+
+    var $ = cheerio.load(sres.text);
+    var items = [ ];
+    $('#topic_list .topic_title').each(function(idx, element) {
+      var $element = $(element);
+      items.push({
+        title : $element.attr('title'),
+        href : $element.attr('href')
+      });
+
+    });
+
+    res.send($element);
+
+  });
+});
+
+app.listen(3000,  function() {
+  console.log('app is listening at port 3000');
+})
