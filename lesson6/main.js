ca5f271e03e43c358fccabea3ca2b4ae32f9ee52
napster99 <morrisonleisure@163.com>
2015-03-02 14:37:09 +0800

    add should

---
+++ lesson6/main.js
@@ -0,0 +1,34 @@
+var fibonacci = function (n) {
+  if (typeof n !== 'number') {
+    throw new Error('n should be a Number');
+  }
+  if (n < 0) {
+    throw new Error('n should >= 0')
+  }
+  if (n > 10) {
+    throw new Error('n should <= 10');
+  }
+  if (n === 0) {
+    return 0;
+  }
+  if (n === 1) {
+    return 1;
+  }
+
+  return fibonacci(n-1) + fibonacci(n-2);
+};
+
+if(require.main === module) {
+	// 如果是直接执行 main.js，则进入此处
+  	// 如果 main.js 被其他文件 require，则此处不会执行。
+  	console.log('>>>>'+ process.argv[0])
+  	console.log('>>>>'+ process.argv[1])
+
+  	for(var i=0; i<10; i++) {
+		var n = Number(process.argv[i+2]);
+		console.log('fibonacci(' + n + ') is', fibonacci(n));
+  		
+  	}
+}
+
+exports.fibonacci = fibonacci;
